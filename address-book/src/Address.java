9f94c4ff7514cae644186ff92d88852f21da2e86
aino-Shubham <shubham@ainosoft.com>
2019-01-09 12:09:18 +0530

    getter and setter shubham

---
+++ address-book/src/Address.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class Address
+{
+	int id , zip ;
+	String  name ,street , city , country;
+	
+	public int getId() {
+		return id;
+	}
+	
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	public int getZip() {
+		return zip;
+	}
+	
+	
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public String getStreet() {
+		return street;
+	}
+	
+	
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	
+	
+	public String getCity() {
+		return city;
+	}
+	
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+	public String getCountry() {
+		return country;
+	}
+	
+	
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+
+
+
+}
