7265fe83174b51530e5223f2601061979070b375
aino-Shubham <shubham@ainosoft.com>
2019-01-09 13:06:39 +0530

    Added operation

---
+++ address-book/src/main/java/com/addressbook/AddressCrudUtil.java
@@ -0,0 +1,29 @@
+package main.java.com.addressbook;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddressCrudUtil
+{
+	Map< String,Address> hm =  new HashMap< String,Address>(); 
+	
+	void create(String name)
+	{
+		hm.put(name, new Address());
+	}
+	
+	void read(String name)
+	{
+		hm.get(name);
+	}
+	
+	void update(String name)
+	{
+		hm.replace(name, new Address());
+	}
+	
+	void delete(String name)
+	{
+		hm.remove(name);
+	}
+}
